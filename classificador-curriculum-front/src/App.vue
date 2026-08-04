3b5d63727af65aa1e3658201da2635bfbb362f61
Rafael <javdt.rapf@gmail.com>
2026-08-04 20:08:41 -0300

    test: testing vue

--- classificador-curriculum-front/src/App.vue
+++ classificador-curriculum-front/src/App.vue
@@ -1,7 +1,9 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import Main from './views/main.vue';
+
+
 </script>
 
 <template>
-  <HelloWorld />
+  <Main />
 </template>
---
+++ classificador-curriculum-front/src/views/Main.vue
@@ -0,0 +1,12 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="main">
+    <h1>Classificador de Currículos</h1>
+    <p>Bem-vindo ao Classificador de Currículos! Esta aplicação permite que você classifique currículos de forma eficiente e organizada.</p>
+    <p>Para começar, clique no botão abaixo para carregar os currículos e iniciar o processo de classificação.</p>
+    <button @click="loadResumes">Carregar Currículos</button>
+  </div>
+</template>
