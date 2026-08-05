d876d1793c0a90c610d3d442690053b5feccd670
Rafael <javdt.rapf@gmail.com>
2026-08-04 21:22:14 -0300

    feat: creating web route

--- classificador-curriculum-front/src/main.js
+++ classificador-curriculum-front/src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import router from './routes/index.js'
+
+app.use(router)
 
 createApp(App).mount('#app')
---
+++ classificador-curriculum-front/src/routes/index.js
@@ -0,0 +1,23 @@
+import { CreateRouter, createWebHistory } from 'vue-router';
+import Main from '../views/main.vue';
+
+const routes = [
+    {
+        path: '/',
+        name: 'Main',
+        component: Main
+    },
+
+    // Path de erro deve ser o último
+    /*{
+        path: '/:pathMatch(.*)*',
+        name: 'not-found',
+        component: () => import('../views/NotFoundView.vue')
+    }*/
+];
+
+const router = CreateRouter({
+    history: createWebHistory, routes
+});
+
+export default router;
