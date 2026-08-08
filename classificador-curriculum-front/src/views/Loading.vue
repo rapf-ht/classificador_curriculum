6da0275dc12df21baf1853a7abbebfab01724657
Rafael <javdt.rapf@gmail.com>
2026-08-08 12:17:03 -0300

    perf: one page, excluding router

--- classificador-curriculum-front/src/views/Loading.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <div class="body">
-        <h1>Carregando Currículo...</h1>
-        <p>Por favor, aguarde enquanto o currículo está sendo processado.</p>
-        <div class="loading-spinner">
-            <img src="../assets/loading-animation.gif" alt="Loading">
-        </div>
-    </div>
-</template>
